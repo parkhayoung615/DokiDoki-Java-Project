4358a255da7d9fb98192d3ab2b40830bd1a2b022
GXWX <93107060+GXWX@users.noreply.github.com>
2021-11-06 17:05:40 +0900

    블럭 오류수정
    오른쪽과 아래로 캐릭터가 삐져나가는거 수정함

--- src/test/MainController.java
+++ src/test/MainController.java
@@ -31,11 +31,11 @@ public class MainController implements Initializable{
         });
 	}
     
-	// 캐릭터 키보드 이동
+	// 罹먮┃�꽣 �궎蹂대뱶 �씠�룞
 	public void moveMario(KeyEvent event) {
         KeyCode keyCode = event.getCode();
         if (keyCode.equals(KeyCode.RIGHT)) {
-        	if (!s.getPassBlock(pass.blockGet(Mario.getX()+10, Mario.getY(), 11), "shop").getPass() && Mario.getX()+10 <= 1100) {
+        	if (!s.getPassBlock(pass.blockGet(Mario.getX()+10, Mario.getY(), 11), "shop").getPass() && Mario.getX()+10 <= 1000) {
         		Mario.setX(Mario.getX() + 10);
         	}
             
@@ -50,7 +50,7 @@ public class MainController implements Initializable{
             }
             
         } else if (keyCode.equals(KeyCode.DOWN)) {
-            if (!s.getPassBlock(pass.blockGet(Mario.getX(), Mario.getY()+10, 11), "shop").getPass() && Mario.getY() <= 1100) {
+            if (!s.getPassBlock(pass.blockGet(Mario.getX(), Mario.getY()+10, 11), "shop").getPass() && Mario.getY() < 800) {
             	Mario.setY(Mario.getY() + 10);
             }
         }
