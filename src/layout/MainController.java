79727b6c878b526e331cbd5e26de6e85ef6ffa67
Hoo21 <waterkite0629@gmail.com>
2021-11-09 14:14:41 +0900

    Update

--- src/layout/MainController.java
+++ src/layout/MainController.java
@@ -4,19 +4,52 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MainController implements Initializable {
-	
+
 	@FXML
 	private Button StartBtn;
+
+	@FXML
+	private Button ChangeJoinBtn;
+	
+	@FXML
+	private Button ChangeLoginBtn;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	public void ChangeJoin() {
+		try {
+			Parent login = FXMLLoader.load(getClass().getResource("/layout/Join.fxml"));
+			Scene scene = new Scene(login);
+			Stage primaryStage = (Stage) ChangeJoinBtn.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+	}
+	
+	public void ChangeLogin() {
+		try {
+			Parent login = FXMLLoader.load(getClass().getResource("/layout/Login.fxml"));
+			Scene scene = new Scene(login);
+			Stage primaryStage = (Stage) ChangeLoginBtn.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+	}
 
 }
