d5268914a22fbfde979146567a386c6d09280d16
박하영 <parkhayoung615@gmail.com>
2021-11-27 18:32:42 +0900

    Update Program

--- src/layout/StartGame/MainController.java
+++ src/layout/StartGame/MainController.java
@@ -27,18 +27,24 @@ public class MainController implements Initializable {
 	private Label NarraLabel;
 	@FXML
 	private Button Next;
+	
+	@FXML
+	private Button NarBtn;
+	@FXML
+	private Label NarLabel;
 
 	private int idx = 0;
 	private ArrayList<String> list = new ArrayList<String>();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		chatLoad("StartNRS");
+		chatLoad("StartNRS1");
+		chatLoad02("StartNRS2");
 	}
 	@FXML
 	public void next1() {
 		try {
-			Parent login = FXMLLoader.load(getClass().getResource("/test/TestLayout.fxml"));
+			Parent login = FXMLLoader.load(getClass().getResource("/layout/StartGame/Start_Min.fxml"));
 			Scene scene = new Scene(login);
 			Stage primaryStage = (Stage) Next.getScene().getWindow();
 			scene.getStylesheets().add(getClass().getResource("/application/application.css").toExternalForm());
@@ -67,7 +73,6 @@ public class MainController implements Initializable {
 			// TODO: handle exception
 		}
 	}
-	
 
 	@FXML
 	public void nextChat() {
@@ -88,5 +93,45 @@ public class MainController implements Initializable {
 			}
 		}
 	}
+	
+	public void chatLoad02(String point) {
+		JDBCUtil db = new JDBCUtil();
+		Connection con = db.getConnection();
+		PreparedStatement pstmt = null;
+		String sql = "SELECT * FROM script s WHERE s.id LIKE ?";
+		ResultSet rs = null;
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, "%" + point + "%");
+			rs = pstmt.executeQuery();
+			System.out.println(1);
+			while (rs.next()) {
+				list.add(rs.getString("s.script_data"));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	@FXML
+	public void nextChat02() {
+		if (idx < list.size()) {
+			NarLabel.setText(list.get(idx));
+			idx++;
+		} else {
+			idx = 0;
+			list.clear();
+			try {
+				Parent login = FXMLLoader.load(getClass().getResource("/layout/StartGame/Start_illustration.fxml"));
+				Scene scene = new Scene(login);
+				Stage primaryStage = (Stage) NarBtn.getScene().getWindow();
+				scene.getStylesheets().add(getClass().getResource("/application/application.css").toExternalForm());
+				primaryStage.setScene(scene);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 
 }
