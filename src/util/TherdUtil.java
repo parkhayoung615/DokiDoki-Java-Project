dc0c30d44e4609f2ed090148ecb55c8d407b6aa8
Hoo21 <waterkite0629@gmail.com>
2021-11-28 20:45:49 +0900

    Update Program

--- src/util/TherdUtil.java
+++ src/util/TherdUtil.java
@@ -1,18 +1,63 @@
 package util;
 
 import java.awt.Toolkit;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 
 public class TherdUtil implements Runnable{
-
-	public TherdUtil(String userId, String user_Boss, String user_Obj) {
-		
+	
+	private static String userId;
+	private String objectId;
+	private String avatarId;
+	ArrayList<String> objs = new ArrayList<String>();
+	public void setUserId(String idd) {
+		userId = idd;
 	}
 
 	@Override
 	public void run() {
-//		String userId;
-//		String user_Boss;
-//		String user_Obj;
+		JDBCUtil db = new JDBCUtil();
+		Connection con = db.getConnection();
+		PreparedStatement pstmt = null;
+		
+		String sql = "SELECY * FROM user_object WHERE AND user_id = ?";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			ResultSet rs = null;
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				objectId = rs.getString("id");
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		sql = "SELECT u.id, d.avatar_id, o.object_id FROM user u, data d, user_object o  WHERE u.id = ? AND u.id = o.user_id LIKE o.id = '%?%'";
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+//			쓰레드 넣어서 가져오기
+			pstmt.setString(1, userId);
+//			위에랑 같음
+			pstmt.setString(2, userId);
+			ResultSet rs = null;
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				objs.add(rs.getString("o.object_id"));
+				avatarId = rs.getString("d.avatar_id");
+			}
+		
+		
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		while(true) {
 //			toolkit.
@@ -22,7 +67,7 @@ public class TherdUtil implements Runnable{
 				// TODO: handle exception
 			}
 		}
-		
+		toolkit.set
 	}
 
 }
